88b7ab64c0c92b86fff42caa10ca2b9e8567136a
holod <holodulinn@mail.ru>
2025-06-15 13:29:03 +0400

    Добавлены тесты FelineTest для класса Feline

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import com.example.Feline;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+
+@ExtendWith(MockitoExtension.class)
 public class FelineTest {
+
+    @Test
+    public void getFoodFelineTest() throws Exception {
+        Feline feline = new Feline();
+        String felineMeat = feline.eatMeat().toString();
+        Assertions.assertEquals("[Животные, Птицы, Рыба]", felineMeat);
+    }
+
+    @Test
+    public void getFamilyFelineTest(){
+        Feline feline = new Feline();
+        String felineFamily = feline.getFamily();
+        Assertions.assertEquals("Кошачьи", felineFamily);
+    }
+
+    @Test
+    public void getKittensWithoutParamsFelineTest(){
+        Feline feline = new Feline();
+        int felineKittens = feline.getKittens();
+        Assertions.assertEquals(1, felineKittens);
+    }
+
+    @Test
+    public void getKittensWithParamsFelineTest(){
+        Feline feline = new Feline();
+        int felineKittens = feline.getKittens(5);
+        Assertions.assertEquals(5, felineKittens);
+    }
+
 }
